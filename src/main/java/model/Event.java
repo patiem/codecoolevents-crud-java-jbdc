d806318afca1b21dcf07d3b43327ba68ac49f02c
Pati Mikulska <pp.mikulska@gmail.com>
2017-05-10 20:56:02 +0200

    Add Evet methods to manipulate date-string

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -1,6 +1,8 @@
 package model;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Event {
@@ -11,6 +13,31 @@ public class Event {
     private EventCategory eventCategory;
     private Integer id;
 
+
+    public static Date makeDateFromString(String dateString) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = null;
+        try {
+            date = formatter.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    public static String simpleStringFromDate(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat( "dd-MM-yyyy");
+        return formatter.format(date);
+    }
+
+    public Boolean isEventAvailable() {
+        Date today = new Date();
+        if (today.before(this.getEventDate())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     public String getName() {
         return name;

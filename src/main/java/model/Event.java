a223da699be5132c7436dde503568ae56e7481e3
Pati Mikulska <pp.mikulska@gmail.com>
2017-05-10 18:39:07 +0200

    Add Event atribiutes, getters and setters for them

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -1,7 +1,54 @@
 package model;
 
-/**
- * Created by pati on 09.05.17.
- */
+
+import java.util.Date;
+
 public class Event {
+
+    private String eventName;
+    private String description;
+    private Date eventDate;
+    private EventCategory eventCategory;
+    private Integer  eventId;
+
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public EventCategory getEventCategory() {
+        return eventCategory;
+    }
+
+    public void setEventCategory(EventCategory eventCategory) {
+        this.eventCategory = eventCategory;
+    }
+
+    public Integer getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Integer eventId) {
+        this.eventId = eventId;
+    }
 }
